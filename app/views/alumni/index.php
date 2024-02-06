7fe40fb7ddb511d2a084e4d5a4afcaf0aaeafbef
nneliifeanyi <stanvicbest@gmail.com>
2024-02-06 15:24:40 +0100

    alumni adjust

--- app/views/alumni/index.php
+++ app/views/alumni/index.php
@@ -14,23 +14,20 @@
   </div>
 </div>
 
-<div class="bg-dark position-fixed bottom-0  px-2 py-3 w-100">
+<div class="bg-dark position-fixed bottom-0  px-2 py-1 w-100">
   <div class="row ">
     <div class="col-md-6 shadow">
-        <h2 class="h4 text-light">Inquiries</h2>
-        <ul class="list-unstyled text-light footer-link-list">
-            <li>
+        <h2 class="h6 text-light">Inquiries</h2>
+        <p>
             <i class="fa fa-envelope fa-fw"></i>
             <a style="text-decoration:none;color:antiquewhite;" href="mailto:mitreaffairs@gmail.com">mitreaffairs@gmail.com</a>
-            </li>
-        </ul>
+           </p> 
     </div>
 
-    <div class="col-md-6 pt-3 ">
+    <div class="col-md-6 pt-1 ">
       <div class="text-center">
         <p class="lead fs-6 text-light">Copyright &copy; <?php echo date('Y');?> <span class="text-white fst-italic fw-semibold">Threshers Team</span>
         <br>
-        <span class=""> All Rights Reserved</span></p>
       </div>  
     </div>
   </div>
