3eb440cfefa550b3de7ddf986ead9184d2db8338
Stanvic <stanvicbest@gmail.com>
2024-09-09 18:17:16 +0100

    yeah2

--- app/views/admin/instructors.php
+++ app/views/admin/instructors.php
@@ -11,7 +11,6 @@
         <div class="col-sm-6">
           <h1 class="m-0">Mitre Instructors Database</h1>
           <p>A Total of <span class="text-primary"><?php echo $data['rowCount']; ?></span> Registered</p>
-          <a href="<?php echo URLROOT; ?>/portal/pdf" class="btn btn-outline-primary">Download Pdf</a>
         </div><!-- /.col -->
         <div class="col-sm-6">
           <ol class="breadcrumb float-sm-right">
@@ -24,7 +23,7 @@
   </div>
   <!-- /.content-header -->
 
-  <style type="text/css">
+  <!-- <style type="text/css">
     .zoom {
       transition: transform .3s;
       border-radius: 50%;
@@ -34,7 +33,7 @@
       transform: scale(2.7);
       border-radius: 0%;
     }
-  </style>
+  </style> -->
 
 
 
@@ -52,16 +51,16 @@
               <tr class="">
                 <th>#</th>
                 <th>Name</th>
-                <th>Photo</th>
-                <th>Gender</th>
+                <!-- <th>Photo</th> -->
+                <!-- <th>Gender</th> -->
                 <th>Zone</th>
                 <th>Address</th>
                 <th>Phone</th>
-                <th>Whatsapp</th>
-                <th>Telegram</th>
+                <!-- <th>Whatsapp</th>
+                <th>Telegram</th> -->
                 <th>Email</th>
                 <th>Ministry</th>
-                <th>Occupation</th>
+                <!-- <th>Occupation</th> -->
                 <th>Assembly</th>
                 <th>Action</th>
 
@@ -74,7 +73,7 @@
                 <tr>
                   <td><?php echo $numbering; ?></td>
                   <td><?php echo $student->name ?></td>
-                  <td>
+                  <!-- <td>
                     <div class="">
                       <?php if (empty($student->photo)) : ?>
                         <img src="<?php echo URLROOT ?>/img/default.png" class="" style="width: 100%;">
@@ -84,18 +83,18 @@
                         </a>
                       <?php endif; ?>
                     </div>
-                  </td>
-                  <td><?php echo $student->gender ?></td>
+                  </td> -->
+                  <!-- <td><?php echo $student->gender ?></td> -->
                   <td><?php echo $student->zone ?></td>
                   <td><?php echo $student->address ?></td>
                   <td><?php echo $student->phone ?></td>
-                  <td><a href="https://wa.me/<?= $student->whatsapp; ?>" class="btn btn-sm"><i class="fab fa-whatsapp" aria-hidden="true"></i> <?php echo $student->whatsapp ?></a></td>
-                  <td><?php echo $student->telegram ?></td>
+                  <!-- <td><a href="https://wa.me/<?= $student->whatsapp; ?>" class="btn btn-sm"><i class="fab fa-whatsapp" aria-hidden="true"></i> <?php echo $student->whatsapp ?></a></td>
+                  <td><?php echo $student->telegram ?></td> -->
                   <td><?php echo $student->email ?></td>
 
 
                   <td><?php echo $student->ministry ?></td>
-                  <td><?php echo $student->occupation ?></td>
+                  <!-- <td><?php echo $student->occupation ?></td> -->
                   <td><?php echo $student->assembly ?></td>
                   <td><a href="<?php echo URLROOT ?>/instructors/edit/<?= $student->id; ?>" class="btn btn-sm btn-success">More</a></td>
                 </tr>
@@ -106,16 +105,16 @@
               <tr class="">
                 <th>#</th>
                 <th>Name</th>
-                <th>Photo</th>
-                <th>Gender</th>
+                <!-- <th>Photo</th> -->
+                <!-- <th>Gender</th> -->
                 <th>Zone</th>
                 <th>Address</th>
                 <th>Phone</th>
-                <th>Whatsapp</th>
-                <th>Telegram</th>
+                <!-- <th>Whatsapp</th>
+                <th>Telegram</th> -->
                 <th>Email</th>
                 <th>Ministry</th>
-                <th>Occupation</th>
+                <!-- <th>Occupation</th> -->
                 <th>Assembly</th>
                 <th>Action</th>
               </tr>
@@ -123,118 +122,10 @@
           </table>
         </div>
       </div><!-- /.container-fluid -->
-<<<<<<< HEAD
   </section>
   <!-- /.content -->
 </div>
 <!-- /.content-wrapper -->
-=======
-    </div>
-    <!-- /.content-header -->
-
-    <style type="text/css">
-        .zoom{
-            transition: transform .3s;
-            border-radius: 50%;
-        }
-
-        .zoom:hover{
-            transform: scale(2.7);
-            border-radius: 0%;
-        }
-    </style>
-
-
-
-
-
-
-    <!-- Main content -->
-    <section class="content">
-      <div class="container-fluid">
-
-      <div class="container-fluid">
-       <div class="card card-body">
-        <?php echo flash('msg');?>
-            <table class="table table-striped" id="example">
-                <thead>
-                   <tr class="">
-                     <th>#</th>
-                     <th>Name</th>
-                     <th>Photo</th>
-                     <th>Gender</th>
-                     <th>Zone</th>
-                     <th>Address</th>
-                     <th>Phone</th>
-                     <th>Whatsapp</th>
-                     <th>Telegram</th>
-                     <th>Email</th>
-                     <th>Ministry</th>
-                     <th>Occupation</th>
-                     <th>Assembly</th>
-                     <th>Action</th>
-                     
-                   </tr>
-                </thead>
-
-                 <tbody>
-                 <?php $numbering = 1; foreach($data['students'] as $student) : ?>
-                  <tr>
-                      <td><?php echo $numbering;?></td>
-                      <td><?php echo $student->name?></td>
-                      <td>
-                        <div class="">
-                            <?php if(empty($student->photo)):?>
-                        <img src="<?php echo URLROOT?>/img/default.png" class="" style="width: 100%;">
-                            <?php else:?>
-                            <a href="<?php echo URLROOT.'/'.$student->photo?>">
-                          <img src="<?php echo URLROOT.'/'.$student->photo?>" class="zoom" style="width: 100%;">
-                            </a>
-                            <?php endif;?>
-                        </div> 
-                      </td>
-                      <td><?php echo $student->gender?></td>
-                      <td><?php echo $student->zone?></td>
-                      <td><?php echo $student->address?></td>
-                      <td><?php echo $student->phone?></td>
-                      <td><a href="https://wa.me/<?= $student->whatsapp;?>" class="btn btn-sm"><i class="fab fa-whatsapp" aria-hidden="true"></i> <?php echo $student->whatsapp?></a></td>
-                      <td><?php echo $student->telegram?></td>
-                      <td><?php echo $student->email?></td>
-                
-
-                      <td><?php echo $student->ministry?></td>
-                      <td><?php echo $student->occupation?></td>
-                      <td><?php echo $student->assembly?></td>
-                      <td><a href="<?php echo URLROOT?>/instructors/edit/<?= $student->id;?>" class="btn btn-sm btn-success">More</a></td>
-                  </tr>
-                  <?php $numbering++; endforeach; ?>
-                 </tbody>
-                  <tfoot>
-                   <tr class="">
-                     <th>#</th>
-                     <th>Name</th>
-                     <th>Photo</th>
-                     <th>Gender</th>
-                     <th>Zone</th>
-                     <th>Address</th>
-                     <th>Phone</th>
-                     <th>Whatsapp</th>
-                     <th>Telegram</th>
-                     <th>Email</th>
-                     <th>Ministry</th>
-                     <th>Occupation</th>
-                     <th>Assembly</th>
-                     <th>Action</th>
-                   </tr>
-                </tfoot>
-               </table>
-       </div>
-        </div><!-- /.container-fluid -->
-      </section>
-      <!-- /.content -->
-    </div>
-    <!-- /.content-wrapper -->
->>>>>>> 9153b4698b519aef918de06e8716d4958797025d
 <?php require APPROOT . '/views/inc/admin/footer.php'; ?>
 
 <script>
